ea04ea6c051abcb318b5c2567b1169ad787f95b9
Leonardo <leonardo@192.168.1.13>
2025-07-28 08:01:16 -0500

    Add LedeLab homepage using global layout

---
+++ src/app/sobre-nosotros/layout.tsx
@@ -0,0 +1,7 @@
+// src/app/ledelab/layout.tsx
+import React from "react";
+import Layout from "@/components/layout";
+
+export default function LedeLabLayout({ children }: { children: React.ReactNode }) {
+  return <Layout>{children}</Layout>;
+}
